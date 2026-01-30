005bd39520d4fead9467be3311f1bb1be8ac90fc
Keith Crooks <keithcrooks@gmail.com>
2026-01-30 19:03:51 +0000

    Add pubsub package and publishJSON

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -21,6 +23,17 @@ func main() {
 	defer con.Close()
 
 	fmt.Println("Connection was successful.")
+
+	ch, err := con.Channel()
+	if err != nil {
+		log.Fatalf("could not create channel: %v", err)
+	}
+
+	val := routing.PlayingState{IsPaused: true}
+	if err := pubsub.PublishJSON(ch, routing.ExchangePerilDirect, routing.PauseKey, val); err != nil {
+		log.Printf("could not publish JSON: %v", err)
+	}
+	fmt.Println("Pause message sent!")
 
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt)
---
+++ internal/pubsub/pubsub.go
@@ -0,0 +1,23 @@
+package pubsub
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
+	data, err := json.Marshal(val)
+	if err != nil {
+		return fmt.Errorf("could not marshal JSON value: %v", err)
+	}
+
+	msg := amqp.Publishing{
+		ContentType: "application/json",
+		Body:        data,
+	}
+
+	return ch.PublishWithContext(context.Background(), exchange, key, false, false, msg)
+}
