510e98422fbc0a265e79971b59ab2c4f20fbc62f
Keith Crooks <keithcrooks@gmail.com>
2026-01-30 23:30:30 +0000

    Add transient queue for user on client startup

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril client...")
+	const url = "amqp://guest:guest@localhost:5672/"
+
+	conn, err := amqp.Dial(url)
+	if err != nil {
+		log.Fatalf("could not connect: %v", err)
+	}
+	defer conn.Close()
+
+	fmt.Println("Connection was successful.")
+
+	username, err := gamelogic.ClientWelcome()
+	if err != nil {
+		log.Println("error getting user name: %v", err)
+	}
+
+	queueName := fmt.Sprintf("%s.%s", routing.PauseKey, username)
+	if _, _, err := pubsub.DeclareAndBind(
+		conn,
+		routing.ExchangePerilDirect,
+		queueName,
+		routing.PauseKey,
+		pubsub.QueueTransient,
+	); err != nil {
+		log.Fatalf("could not declare and bind: %v", err)
+	}
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+
+	fmt.Println("Closing Peril client...")
 }
--- internal/pubsub/pubsub.go
+++ internal/pubsub/pubsub.go
@@ -8,6 +8,47 @@ import (
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
+type SimpleQueueType int
+
+const (
+	QueueDurable SimpleQueueType = iota
+	QueueTransient
+)
+
+func DeclareAndBind(
+	conn *amqp.Connection,
+	exchange,
+	queueName,
+	key string,
+	queueType SimpleQueueType, // an enum to represent "durable" or "transient"
+) (*amqp.Channel, amqp.Queue, error) {
+	ch, err := conn.Channel()
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+
+	var durable, autoDelete, exclusive bool
+
+	switch queueType {
+	case QueueDurable:
+		durable = true
+		autoDelete = false
+		exclusive = false
+	case QueueTransient:
+		durable = false
+		autoDelete = true
+		exclusive = true
+	}
+
+	q, err := ch.QueueDeclare(queueName, durable, autoDelete, exclusive, false, nil)
+
+	if err := ch.QueueBind(q.Name, key, exchange, false, nil); err != nil {
+		return ch, q, err
+	}
+
+	return ch, q, nil
+}
+
 func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
 	data, err := json.Marshal(val)
 	if err != nil {
