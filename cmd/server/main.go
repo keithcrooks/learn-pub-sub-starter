f91677a050d7edd502f8c55267dece4a87444986
Keith Crooks <keithcrooks@gmail.com>
2026-01-30 17:58:55 +0000

    Add RabbitMQ connection to server main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+
+	const url = "amqp://guest:guest@localhost:5672/"
+
+	con, err := amqp.Dial(url)
+	if err != nil {
+		log.Fatalf("could not connect: %v", err)
+	}
+	defer con.Close()
+
+	fmt.Println("Connection was successful.")
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+
+	fmt.Println("Closing Peril server...")
 }
